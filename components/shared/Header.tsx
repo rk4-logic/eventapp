dc81d2124eb033134de4b82bf5cc9118f8424fc9
Rohit Prajapati <rhtprjpt54@gmail.com>
2024-06-08 12:31:19 +0530

    Hydration error solved

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import { Button } from "../ui/button"
 
 const Header = () => {
-  
+
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
@@ -15,17 +15,18 @@ const Header = () => {
             height={38}
             alt="Eventndler Logo"
           />
-
-          <div className="flex w-32 justify-end gap-3">
-            <SignedOut>
-              <Button asChild className="rounded-full" size='lg' >
-                <Link href='/sign-in' >
-                  Login
-                </Link>
-              </Button>
-            </SignedOut>
-          </div>
         </Link>
+        
+        <div className="flex w-32 justify-end gap-3">
+          <SignedOut>
+            <Button asChild className="rounded-full" size='lg' >
+              <Link href='/sign-in' >
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
+        </div>
+
 
       </div>
     </header>
